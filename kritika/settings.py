86e662dfa4562b6d3d66b85b4e22f877f855ff16
gleb-skobinsky <glebgytnik@gmail.com>
2023-03-30 17:49:22 +0300

    prepare deploy

--- kritika/settings.py
+++ kritika/settings.py
@@ -87,6 +87,7 @@ WSGI_APPLICATION = "kritika.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "URL": os.environ.get("DATABASE_URL"),
         "NAME": os.environ.get("DATABASE_NAME"),
         "USER": os.environ.get("DATABASE_USER"),
         "PASSWORD": os.environ.get("DATABASE_PASSWORD"),

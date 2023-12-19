571c9467b9b75bae1589b7bf30684a9e92614b5f
gleb-skobinsky <glebgytnik@gmail.com>
2023-12-20 00:33:14 +0300

    Try to fix admin static files

--- kritika/urls.py
+++ kritika/urls.py
@@ -20,13 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns = (
-    [
+urlpatterns = [
         path("admin/", admin.site.urls),
         path("", home, name="main-view"),
         path("", include("kritika_main.urls")),
-    ]
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + staticfiles_urlpatterns()
-)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()

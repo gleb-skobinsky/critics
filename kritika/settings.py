fc3aab89675ff2c2758bb036f355857495ee9b3b
gleb-skobinsky <glebgytnik@gmail.com>
2023-12-19 23:54:11 +0300

    Try to fix admin static files

--- kritika/settings.py
+++ kritika/settings.py
@@ -184,8 +184,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "media/"
-
-MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 CKEDITOR_CONFIGS = {
